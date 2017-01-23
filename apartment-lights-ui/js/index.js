922e8f1367d9028d1b954c03d6cbf5f09c2a341e
Andrew Rabert <draje@nullsum.net>
2017-01-22 20:26:46 -0500

    Update for nvllsvm/NeoPixel_REST_WiFi

--- apartment-lights-ui/js/index.js
+++ apartment-lights-ui/js/index.js
@@ -1,32 +1,41 @@
 $(document).ready(() => {
-    const baseURL = 'http://atlas/lights/';
-
-    const lightMethodMap = { white: 'L06',
-                             blue: 'L04',
-                             red: 'L00',
-                             green: 'L02',
-                             rainbow: 'L05',
-                             off: 'L14' };
+    const baseURL = 'http://atlas/lights';
 
     function setupLightGroup(local) {
         const template = $(document.getElementById('light-group').content).clone();
-        template.find('.button').data('lightSet', local);
-        template.find('.group-header').text(local);
+        template.find('.button').data('paths', local.paths);
+        template.find('.group-header').text(local.name);
 
-        $.map(lightMethodMap, (value, key) => {
-            template.find(`#${key}.button`).data('method', value);
-        });
+        template.find(`#red.button`).data('args', {'mode': 'color', 'red': 255});
+        template.find(`#blue.button`).data('args', {'mode': 'color', 'blue': 255});
+        template.find(`#green.button`).data('args', {'mode': 'color', 'green': 255});
+        template.find(`#white.button`).data('args', {'mode': 'color', 'white': 255, 'blue': 255});
+        template.find(`#rainbow.button`).data('args', {'mode': 'rainbow', 'interval': 0});
+        template.find(`#off.button`).data('args', {'mode': 'off'});
 
         $(document.body).append(template);
     }
 
-    const groups = ['bedroom', 'tv'];
+    const groups = [{'name': 'bedroom'
+                    ,'paths': ['bedroom/short', 'bedroom/long']}
+                   ,{'name': 'monitor'
+                    ,'paths': ['monitor']}
+                   ,{'name': 'windowsill'
+                    ,'paths': ['windowsill']}]
+
     groups.forEach(setupLightGroup);
 
     $('.button').on('click', (event) => {
-        const method = $(event.target).data('method');
-        const lightSet = $(event.target).data('lightSet');
+        const data = $(event.target).data();
+        console.log(data.paths);
+        const color = $(event.target).data('blue');
 
-        $.get(`${baseURL}${lightSet}?method=${method}`);
+        data.paths.forEach((path)=>{
+            $.ajax({
+                url: 'lights/' + path,
+                method: 'PUT',
+                data: JSON.stringify(data.args)
+            });
+        });
     });
 });
